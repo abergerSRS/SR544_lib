ef547523ea77cdece3a7ae905020f675d33dccfe
abergerSRS <aberger@thinksrs.com>
2020-03-23 14:25:46 -0700

    bringing files up to date for fp sim

--- motorDrive_win32.c
+++ motorDrive_win32.c
@@ -4,6 +4,14 @@
 
 #include "motorDrive.h"
 
+float torqueScale = 1.0f;
+uint16_t rotorAdjust = 0;
+float compScale = 1.0f;
+uint32_t compAdjust = 0;
+
+uint32_t motorFtmPdSpDnCnt = 0; // used during spin-down measurements
+uint32_t ddsFtmPdSpDnCnt = 0;
+
 uint8_t get_N_outer(void) {
 	return 6;
 }
