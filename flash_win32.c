606e964b1649bea32e2f818ec582cee2517fecfd
abergerSRS <aberger@thinksrs.com>
2020-03-25 08:57:25 -0700

    changed indexing of stored flash configs

--- flash_win32.c
+++ flash_win32.c
@@ -4,13 +4,13 @@
 #include "instrument.h"
 #include <stdint.h>
 
-static uint32_t savedConfig[SAVED_CONFIG_SIZE][9];
+static uint32_t savedConfig[SAVED_CONFIG_SIZE][10];
 
 void flashWrite(int configNum, uint32_t * configBuffer, unsigned int size)
 {
 	int i;
 	for (i = 0; i < SAVED_CONFIG_SIZE; i++) {
-		savedConfig[i][configNum-1] = configBuffer[i];
+		savedConfig[i][configNum] = configBuffer[i];
 	}
 }
 
@@ -18,7 +18,7 @@ void flashRead(int configNum, uint32_t * configBuffer, unsigned int size)
 {
 	int i;
 	for (i = 0; i < SAVED_CONFIG_SIZE; i++) {
-		configBuffer[i] = savedConfig[i][configNum-1];
+		configBuffer[i] = savedConfig[i][configNum];
 	}
 }
 
