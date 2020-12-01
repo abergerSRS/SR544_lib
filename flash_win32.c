04f02cc445cab4cf7644bcc4a051cbb6d8d863a2
Andy Berger <aberger@corp.thinksrs.com>
2020-12-01 14:37:37 -0800

    refactor flash_win32 to use a simulated flash block of uint8_t's

--- flash_win32.c
+++ flash_win32.c
@@ -2,24 +2,62 @@
 
 #include "flash_kinetis.h"
 #include "instrument.h"
+#include "status.h"
 #include <stdint.h>
+#include <assert.h>
 
-static uint32_t savedConfig[SAVED_CONFIG_SIZE][10];
+#define FLASH_SECTOR_SIZE   0x1000
+#define FLASH_TOTAL_SIZE    0x00100000
+#define FLASH_BLOCK_SIZE	0x00080000
+#define CONFIG_0_ADDR       0x000F5000
+#define NUM_CONFIG			10
+#define SAVED_CAL_SIZE_BYTES (SAVED_CAL_STRUCT_SIZE_LWORDS*sizeof(uint32_t))
 
-void flashWrite(int configNum, uint32_t * configBuffer, unsigned int size)
+static uint8_t simulatedFlash[FLASH_BLOCK_SIZE];
+
+void ReadFlashWords(uint32_t* destBuffer, uint32_t startAddress, size_t numWords)
 {
-	int i;
-	for (i = 0; i < SAVED_CONFIG_SIZE; i++) {
-		savedConfig[i][configNum] = configBuffer[i];
+	// Make sure start address in program flash block 2 (0x0008000 to 0x00FFFFF)
+	if (startAddress < 0x0008000 || startAddress >= 0x00FFFFF) {
+		RecordError(EXE_RECALL_FAILED);
 	}
+
+	// But the simulated flash starts at address 0
+	startAddress -= FLASH_BLOCK_SIZE;
+	memcpy(destBuffer, &simulatedFlash[startAddress], numWords * 4);
 }
 
-void flashRead(int configNum, uint32_t * configBuffer, unsigned int size)
+int WriteFlash(uint32_t* srcData, uint32_t destAddress, size_t numBytes)
 {
-	int i;
-	for (i = 0; i < SAVED_CONFIG_SIZE; i++) {
-		configBuffer[i] = savedConfig[i][configNum];
+	// Make sure destAddress in program flash block 2 (0x0008000 to 0x00FFFFF)
+	if ((destAddress < 0x0008000) ||
+		(destAddress >= 0x00FFFFF) ||
+		(destAddress & 0x7 != 0)) {
+		return EXE_SAVE_FAILED;
 	}
+
+	// But the simulated flash starts at address 0
+	destAddress -= FLASH_BLOCK_SIZE;
+	memcpy(&simulatedFlash[destAddress], (uint8_t*)srcData, numBytes);
+
+	return 0;
+}
+
+uint32_t GetConfigAddress(int configNum)
+{
+	int32_t address = CONFIG_0_ADDR + (configNum)*FLASH_SECTOR_SIZE;
+	assert(address < FLASH_TOTAL_SIZE);
+
+	return address;
+}
+
+uint32_t GetCalAddress(void)
+{
+	// Cal data is stored 1 sector * before * config 0
+	uint32_t address = CONFIG_0_ADDR - FLASH_SECTOR_SIZE;
+	assert(address < FLASH_TOTAL_SIZE);
+
+	return address;
 }
 
 void CalculateFlashStartAddr(void)
